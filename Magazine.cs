4d3ba6255c9a00fb98561f78ffe986872f20c409
Funclose <ar.doroxoff@gmail.com>
2024-07-04 14:35:59 +0300

    end

---
+++ Magazine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace hw_11_07
+{
+    internal class Magazine
+    {
+        public string Name {  get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public int Pages { get; set; }
+
+        public Magazine(string name, string author, int year, int pages)
+        {
+            Name = name;
+            Author = author;
+            Year = year;
+            Pages = pages;
+        }
+
+        public override string ToString() => $"{Name}, {Author}, {Year}, {Pages}";
+
+
+        public void Show()
+        {
+            Console.WriteLine(this);
+        }
+
+        public static void SerializedMagazine(List<Magazine> list, string filepath)
+        {
+            var option = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            string jsonfile = JsonSerializer.Serialize(list,option);
+            File.WriteAllText(filepath,jsonfile);
+        }
+
+        public static List<Magazine> DeserializeMagazine(string filepath)
+        {
+            string json = File.ReadAllText(filepath);
+            return JsonSerializer.Deserialize<List<Magazine>>(json);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace hw_11_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Magazine> magazines = new List<Magazine>()
+            {
+                new Magazine("Lana","gracy",1954,144),
+                new Magazine("micro","Lasav",1977,190),
+                new Magazine("something","Linkol",2003,155),
+                new Magazine("usualy","osaber",1855,1242),
+                new Magazine("already","ghsdg",2000,115)
+            };
+
+            string filepath = @".\myfile.json";
+            Console.WriteLine("список журналов");
+            foreach (var magazine in magazines)
+            {
+                magazine.Show();
+            }
+
+            Magazine.SerializedMagazine(magazines, filepath);
+           
+            var desserialize = Magazine.DeserializeMagazine(filepath);
+            foreach (var magazine in desserialize)
+            {
+                magazine.Show();
+            }
+        }
+    }
+}
